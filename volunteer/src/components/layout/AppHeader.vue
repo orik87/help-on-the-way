224535acbc1d2bf34f4af346312253b7773fbc30
Ori Koral <orikoral@yahoo-inc.com>
2018-06-13 14:35:08 +0300

    add avatar to header

--- volunteer/src/components/layout/AppHeader.vue
+++ volunteer/src/components/layout/AppHeader.vue
@@ -1,9 +1,14 @@
 <template>
   <v-toolbar color="white" light fixed app>
-    <v-avatar>
-      <h4>5823*</h4>
-      <!-- <img src="/static/apple-touch-icon-180x180.png" alt="avatar"> -->
+    <v-avatar
+          :tile=false
+          :size="48"
+          color="grey lighten-4"
+          class="mr-2"
+        >
+      <img src="https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=104754930364432&height=50&width=50&ext=1529148216&hash=AeRYRkJ2-Oc-FNK6">
     </v-avatar>
+    <h4>5823*</h4>
     <v-spacer></v-spacer>
     <v-toolbar-title class="top-title"  @click="$router.push('/')">עזרה בדרך</v-toolbar-title>
     <v-spacer></v-spacer>
